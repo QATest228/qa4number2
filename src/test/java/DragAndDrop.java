5ef7f1340d25bbfba447a8357160e962dd546a6e
Vaqif Yusibli <vaqifyusibli@gmail.com>
2024-12-06 21:02:48 +0400

    DragAndDrop

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+
+public class DragAndDrop {
+    @Test
+    void DragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
