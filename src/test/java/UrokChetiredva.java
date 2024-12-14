5e47353d9c3ee1dd46b5b97e2bfdb96108e5ad15
QATest228 <153380734+QATest228@users.noreply.github.com>
2024-12-14 21:26:49 +0400

    Update UrokChetiredva.java

--- src/test/java/UrokChetiredva.java
+++ src/test/java/UrokChetiredva.java
@@ -9,7 +9,6 @@ public class UrokChetiredva {
 
     @Test
     void Solution() {
-        Configuration.holdBrowserOpen = true; // Оставляем окно браузера открытым
         open("https://github.com/home");
         $(byTagAndText("button", "Solutions")).hover();
         $("li a[href='https://github.com/enterprise']").shouldHave(text("Enterprises")).click();
